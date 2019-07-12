df665c883283cf550bd2ee71d5bf274b70e2baed
Chen <zdms1987@hotmail.com>
2019-07-12 17:07:44 -0500

    setup shutter and motor

--- seis-idd/tomo_init.py
+++ seis-idd/tomo_init.py
@@ -20,18 +20,6 @@ from utility import print_dict
 list_predefined_vars = lambda : print_dict(keywords_vars)
 list_predefined_func = lambda : print_dict(keywords_func)
 
-# def list_predefined_vars():
-#     print(f"🙊: These are the predefined vars:")
-#     for key, val in keywords_vars.items():
-#         print(f"\t{key}:\t{val}")
-#     print()
-
-# def list_predefined_func():
-#     print(f"🙊: These are the predefined functions:")
-#     for key, val in keywords_func.items():
-#         print(f"\t{key}:\t{val}")
-#     print()
-
 
 # --- get system info
 HOSTNAME = socket.gethostname() or 'localhost'
@@ -49,8 +37,8 @@ keywords_vars['metadata_db'] = 'Default metadata handler'
 # --- setup RunEngine
 from bluesky import RunEngine
 from bluesky.callbacks.best_effort import BestEffortCallback
-keywords_func['getRunEngine'] = 'Get a bluesky RunEngine'
-def getRunEngine(db=None):
+keywords_func['get_runengine'] = 'Get a bluesky RunEngine'
+def get_runengine(db=None):
     """
     Return an instance of RunEngine.  It is recommended to have only
     one RunEngine per session.
@@ -61,7 +49,7 @@ def getRunEngine(db=None):
     RE.subscribe(BestEffortCallback())
     return RE
 
-RE = getRunEngine()
+RE = get_runengine()
 keywords_vars['RE'] = 'Default RunEngine instance'
 
 
@@ -70,10 +58,92 @@ from utility import load_config
 keywords_func['load_config'] = load_config.__doc__
 
 
+# --- load devices info from yaml file
+_devices = load_config('seis-idd/devices.yml')
 
 
+# --- exp safeguard suspender
+from bluesky.suspenders import SuspendFloor
 
 
+# --- define shutter
+keywords_func['get_shutter'] = 'Return a connection to a sim/real shutter'
+def get_shutter(mode='debug'):
+    """
+    return
+        simulated shutter <-- dryrun, debug
+        acutal shutter    <-- production
+    """
+    import apstools.devices as APS_devices
+    aps = APS_devices.ApsMachineParametersDevice(name="APS")
 
+    if mode.lower() in ['debug', 'dryrun']:
+        A_shutter = APS_devices.SimulatedApsPssShutterWithStatus(name="A_shutter")
+    elif mode.lower() == 'production':
+        A_shutter = APS_devices.ApsPssShutterWithStatus(
+            _devices['A_shutter'][0],
+            _devices['A_shutter'][1],
+            name="A_shutter",
+        )
+        suspend_APS_current = SuspendFloor(aps.current, 2, resume_thresh=10)
+        RE.install_suspender(suspend_APS_current)
+    else:
+        raise ValueError(f"🙉: invalide mode, {mode}")
+
+    return A_shutter
+
+A_shutter = get_shutter(mode='debug')
+keywords_vars['A_shutter'] = "shutter instance"
+# no scans until A_shutter is open
+suspend_A_shutter = None  # place holder
+keywords_vars['suspend_A_shutter'] = "no scans until A_shutter is open"
+
+
+# --- define motor
+from ophyd import MotorBundle
+from ophyd import Component
+from ophyd import EpicsMotor
+from ophyd import sim
+
+class TomoStage(MotorBundle):
+    #rotation
+    preci = Component(EpicsMotor, _devices['tomo_stage']['preci'], name='preci')    
+    samX  = Component(EpicsMotor, _devices['tomo_stage']['samX' ], name='samX' )
+    ksamx = Component(EpicsMotor, _devices['tomo_stage']['ksamx'], name='ksamx')
+    samY  = Component(EpicsMotor, _devices['tomo_stage']["samY" ], name="samY" )
+
+keywords_func['get_motors'] = 'Return a connection to sim/real tomostage motor'
+def get_motors(mode="debug"):
+    """
+    sim motor <-- debug
+    aerotech  <-- dryrun, production
+    """
+    if mode.lower() == 'debug':
+        tomostage = TomoStage(name='tomostage')
+    elif mode.lower() in ['dryrun', 'production']:
+        tomostage = MotorBundle(name="tomostage")
+        tomostage.preci = sim.motor
+        tomostage.samX = sim.motor
+        tomostage.samY = sim.motor
+    else:
+        raise ValueError(f"🙉: invalide mode, {mode}")
+    return tomostage
+
+tomostage = get_motors(mode='debug')
+keywords_vars['tomostage'] = 'sim/real tomo stage'
+preci = tomostage.preci
+keywords_vars['preci'] = 'rotation control'
+samX = tomostage.samX
+keywords_vars['samX'] = 'tomo stage x-translation'
+ksamx = tomostage.ksamx
+keywords_vars['ksamx'] = '?'
+samY = tomostage.samY
+keywords_vars['samY'] = 'tomo stage y-translation'
+
+
+# --- define psofly control 
+
+
+# --- define detector
 
 
