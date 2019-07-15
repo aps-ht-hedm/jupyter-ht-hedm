17c147b96c2d678c59b8b28f2ef3fb135c81efde
Chen <zdms1987@hotmail.com>
2019-07-15 14:22:41 -0500

    add detector init
    Use 6BM as testing example

--- seis-idd/tomo_init.py
+++ seis-idd/tomo_init.py
@@ -204,5 +204,92 @@ keywords_vars['psofly'] = 'fly control instance'
 
 
 # --- define detector
+from ophyd   import AreaDetector
+from ophyd   import SingleTrigger, EpicsSignalWithRBV
+from ophyd   import ADComponent
+from ophyd   import PointGreyDetectorCam
+from ophyd   import ProcessPlugin
+from ophyd   import TIFFPlugin
+from ophyd   import HDF5Plugin
+from ophyd   import sim
+from pathlib import Path
+import epics
 
+class PointGreyDetectorCam6IDD(PointGreyDetectorCam):
+    """PointGrey Grasshopper3 cam plugin customizations (properties)"""
+    auto_exposure_on_off = ADComponent(EpicsSignalWithRBV, "AutoExposureOnOff")
+    auto_exposure_auto_mode = ADComponent(EpicsSignalWithRBV, "AutoExposureAutoMode")
+    sharpness_on_off = ADComponent(EpicsSignalWithRBV, "SharpnessOnOff")
+    sharpness_auto_mode = ADComponent(EpicsSignalWithRBV, "SharpnessAutoMode")
+    gamma_on_off = ADComponent(EpicsSignalWithRBV, "GammaOnOff")
+    shutter_auto_mode = ADComponent(EpicsSignalWithRBV, "ShutterAutoMode")
+    gain_auto_mode = ADComponent(EpicsSignalWithRBV, "GainAutoMode")
+    trigger_mode_on_off = ADComponent(EpicsSignalWithRBV, "TriggerModeOnOff")
+    trigger_mode_auto_mode = ADComponent(EpicsSignalWithRBV, "TriggerModeAutoMode")
+    trigger_delay_on_off = ADComponent(EpicsSignalWithRBV, "TriggerDelayOnOff")
+    frame_rate_on_off = ADComponent(EpicsSignalWithRBV, "FrameRateOnOff")
+    frame_rate_auto_mode = ADComponent(EpicsSignalWithRBV, "FrameRateAutoMode")
+
+class HDF5Plugin6IDD(HDF5Plugin):
+    """AD HDF5 plugin customizations (properties)"""
+    xml_file_name = ADComponent(EpicsSignalWithRBV, "XMLFileName")
+
+class PointGreyDetector6IDD(SingleTrigger, AreaDetector):
+    """Point Gray area detector used at 6IDD"""
+    # cam component
+    cam = ADComponent(PointGreyDetectorCam6IDD, "cam1:")
+    # proc plugin
+    proc1 = ADComponent(ProcessPlugin, suffix="Proc1:")
+    # tiff plugin
+    tiff1 = ADComponent(TIFFPlugin, suffix="TIFF1:")
+    # HDF5 plugin
+    hdf1 = ADComponent(HDF5Plugin6IDD, suffix="HDF1:")
+
+keywords_func['get_detector'] = 'Return a connection to the detector'
+def get_detector(
+    mode='debug', 
+    ADPV_prefix=_devices['area_detector']['ADPV_prefix'],
+    ):
+    """
+    sim det  <-- debug
+    PG2      <-- dryrun, production
+    """
+    if mode.lower() == 'debug':
+        det = sim.noisy_det
+    elif mode.lower() in ['dryrun', 'production']:
+        det = PointGreyDetector6IDD(f"{ADPV_prefix}:", name='det')
+
+        # we need to manually setup the PVs to store background and projections
+        # separately in a HDF5 archive
+        # this is the PV we use as the `SaveDest` attribute
+        # check the following page for important information
+        # https://github.com/BCDA-APS/use_bluesky/blob/master/notebooks/sandbox/images_darks_flats.ipynb
+        #
+        epics.caput(f"{ADPV_prefix}:cam1:FrameType.ZRST", "/exchange/data_white_pre")
+        epics.caput(f"{ADPV_prefix}:cam1:FrameType.ONST", "/exchange/data")
+        epics.caput(f"{ADPV_prefix}:cam1:FrameType.TWST", "/exchange/data_white_post")
+        epics.caput(f"{ADPV_prefix}:cam1:FrameType.THST", "/exchange/data_dark")
+        # ophyd needs this configuration
+        epics.caput(f"{ADPV_prefix}:cam1:FrameType_RBV.ZRST", "/exchange/data_white_pre")
+        epics.caput(f"{ADPV_prefix}:cam1:FrameType_RBV.ONST", "/exchange/data")
+        epics.caput(f"{ADPV_prefix}:cam1:FrameType_RBV.TWST", "/exchange/data_white_post")
+        epics.caput(f"{ADPV_prefix}:cam1:FrameType_RBV.THST", "/exchange/data_dark")
+        # set the layout file for cam
+        det.cam.nd_attributes_file.put(str(Path(_devices['area_detector']['cam_layout']).absolute()))
+        # set attributes for HDF5 plugin
+        det.hdf1.xml_file_name.put(str(Path(_devices['area_detector']['hdf_layout']).absolute()))
+        # turn off the problematic auto setting in cam
+        det.cam.auto_exposure_auto_mode.put(0)  
+        det.cam.sharpness_auto_mode.put(0)
+        det.cam.gain_auto_mode.put(0)
+        det.cam.frame_rate_auto_mode.put(0)
+    else:
+        raise ValueError(f"🙉: invalide mode, {mode}")
+
+    return det
+
+det = get_detector(mode='debug')
+keywords_vars['det'] = 'Area detector instance'
+
+print(f'leaving {__file__}...\n')
 
