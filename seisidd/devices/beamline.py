746d7fe875ed9469841db84f7a9c8ee4b5840626
Chen <zdms1987@hotmail.com>
2019-08-15 12:57:45 -0500

    add corase structure of beam def

--- seisidd/devices/beamline.py
+++ seisidd/devices/beamline.py
@@ -4,5 +4,57 @@
 This module contains beamline specific control macros and functions.
 """
 
+from ophyd   import MotorBundle
+from ophyd   import EpicsMotor
+from ophyd   import Component
+
+
+class Slits(MotorBundle):
+    """Control the four blades that form the beam"""
+
+    left   = Component(EpicsMotor, "PV_left",   name='left'  )
+    right  = Component(EpicsMotor, "PV_right",  name='right' )
+    top    = Component(EpicsMotor, "PV_top",    name='top'   )
+    bottom = Component(EpicsMotor, "PV_bottom", name='bottom')
+
+
+class Attenuator:
+    """Attenuator control"""
+    # TODO:
+    #   Lack of sufficient information to implement
+    #   * set_atten() ?? 
+    pass
+
+
+class Beam:
+    """Provide full control of the beam."""
+
+    def __init__(self):
+        self.slits = Slits(name='slits')
+        self.atten = Attenuator()         # adjustment needed
+
+    @property
+    def status(self):
+        """Return full beam status"""
+        pass
+
+    @property
+    def center(self):
+        """return estimated center based on slits positions"""
+        pass
+
+
+class EnergyFoil:
+    """Energy foil"""
+    # TODO:
+    #   Requir acutal implementation once the nature of the device is known
+    pass
+
+
+class Counter:
+    """Counter"""
+    pass
+
+
 if __name__ == "__main__":
     pass
