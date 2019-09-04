2a3fd5a9f2d13e4652aad1c88645b35fe12f3805
JasonZ <jasonzhouquan@gmail.com>
2019-09-04 11:07:33 -0500

    Update beamline.py
    cosmetic changes

--- seisidd/devices/beamline.py
+++ seisidd/devices/beamline.py
@@ -192,8 +192,8 @@ class Beam:
 
     #   I'm not quite sure if it is safe to do all this here. /JasonZ
     def __init__(self):
-        self.s1      = SlitUpstream()
-        self.s2      = SlitDownstream()
+        self.s1         = SlitUpstream()
+        self.s2         = SlitDownstream()
         self.l1         = FocusLens1()
         self.l2         = FocusLens2()
         self.l3         = FocusLens3()
