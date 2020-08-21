62113f2a249b8530406d074a949e0cf9680821db
s6hedm <s6hedm@lyapunov.xray.aps.anl.gov>
2020-08-21 10:08:42 -0500

    add main shutter

--- seisidd/devices/beamline.py
+++ seisidd/devices/beamline.py
@@ -8,6 +8,32 @@ from ophyd   import Device
 from ophyd   import MotorBundle
 from ophyd   import EpicsMotor
 from ophyd   import Component
+from ophyd   import EpicsSignal, EpicsSignalRO
+from apstools.devices import ShutterBase
+
+class MainShutter6IDD(ShutterBase):
+    open_signal  = Component(EpicsSignal, "AcO8")
+    close_signal = Component(EpicsSignal, "AcO7")
+    state_signal = EpicsSignalRO("PA:06ID:STA_D_SDS_OPEN_PL.VAL")
+    delay_time   = 1.5  # seconds
+    
+    @property
+    def state(self):
+        return self.state_signal.get()
+    
+    def open(self, timeout=10):
+        self.open_signal.put(1)  # send out open request
+        if self.delay_time > 0:
+            sleep(self.delay_time)
+        if self.open_signal.get() == 1:
+            self.open_signal.put(0)  # cancel the open request, shutter is open now
+    
+    def close(self, timeout=10):
+        self.close_signal.put(1)  # send out the close request
+        if self.delay_time > 0:
+            sleep(self.delay_time)
+        if self.close_signal.get() == 1:
+            self.close_signal.put(0)  # cancle the close request, shutter is closed now
 
 
 class SlitUpstream(MotorBundle):
